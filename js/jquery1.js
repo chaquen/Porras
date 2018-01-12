2c00910359c5a610bb95d27b22dcb07b2460a86f
StalinDev <31050469+StalinDev@users.noreply.github.com>
2018-01-12 18:50:08 -0500

    Inicio de Proyecto en Git
    Se sube el proyecto a Git

---
+++ js/jquery1.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $('#fotoSer1').click(function(){
+        $('#servicio1').fadeIn('slow');
+    });
+    $('#fotoSer2').click(function(){
+        $('#servicio2').fadeIn('slow');
+    });
+    $('#fotoSer3').click(function(){
+        $('#servicio3').fadeIn('slow');
+    });
+    $('#fotoSer4').click(function(){
+        $('#servicio4').fadeIn('slow');
+    });
+    $('#fotoSer5').click(function(){
+        $('#servicio5').fadeIn('slow');
+    });
+    $('#fotoSer6').click(function(){
+        $('#servicio6').fadeIn('slow');
+    });
+    
+    $('.salirMas').click(function(){
+        $('.mascara').fadeOut('fast');
+    });
+});
+    
+    
